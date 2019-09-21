5abc62c1597b30ce73eb5005aa0fff2d3f091dfc
Italo Fernandes <italogsfernandes@gmail.com>
2019-09-21 18:39:19 -0300

    first views

--- vitalis/admin.py
+++ vitalis/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import MultiparametricReading
+
+class MultiparametricReadingAdmin(admin.ModelAdmin):
+    model = MultiparametricReading
+    list_display = [
+        'user',
+        'created_date',
+        'heart_rate',
+        'oxygen_saturation',
+        'body_temperature'
+    ]
+    readonly_fields = ['created_date']
+
+admin.site.register(MultiparametricReading, MultiparametricReadingAdmin)
+
